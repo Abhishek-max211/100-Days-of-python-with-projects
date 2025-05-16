9f775c667d56556a81ac832c534c1c4e171c649b
Abhishek Pundir <abhishek.pundir29nov@gmail.com>
2025-05-16 11:37:16 +0530

    Add files via upload
    this folder is all about 100 days of python with 100 projects

---
+++ day_1.py
@@ -0,0 +1,27 @@
+# Day 1
+# Printing to the console in python by using print function
+print("Hello World!")
+print("I am Abhishek Pundir")
+print("I am learning Python")
+
+# String Manipulation and code Intelligence
+# Escape sequence in python
+print("Hello Everyone\nI am Abhishek\nand I am using escape sequence")
+# How to do String Concatenation
+print("Hello"+" "+"World")
+print("Hello " + "Everyone")
+
+# Input Function in python
+print("Welcome to my code")
+print("Hello"+" "+ input("What is your name?")+"!")
+
+# Variables in python
+name="Abhishek Pundir"
+print("I am "+ name)
+value = 53
+print(value)
+num = 23.44
+print(num)
+print(value + num)
+print("Thank You for Visiting")
+# Variable can store any kind of value
---
+++ day_1_Project_1.py
@@ -0,0 +1,5 @@
+# Project-1 :- Band name generator
+print("Welcome to the Band Name Generator.")
+city = input("Which city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
+print("Your band name could be: " + city + " " + pet)
