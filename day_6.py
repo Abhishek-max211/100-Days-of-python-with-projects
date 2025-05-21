731357127e10cbf66e35c24163601b730be7dcdd
Abhishek Pundir <abhishek.pundir29nov@gmail.com>
2025-05-21 17:28:37 +0530

    Add files via upload

---
+++ day_6.py
@@ -0,0 +1,38 @@
+# Day 6
+# Funtions in Python
+
+# Defining the function
+def my_function():
+    print("Hello Everyone!")
+    print("I am Abhishek Pundir")
+    print("I am using Function in python",end=" ")
+
+# Calling the function
+my_function()
+
+# Function of Greeting
+def greeting():
+    user=input("What is your pretty Name? ")
+    print(f"Welcome {user} Have a Good Day!")
+
+# Call the function 
+greeting()
+
+# Function to add number
+def add():
+    a=5
+    b=5
+    print(f"The First Number is {a}")
+    print(f"The Second Number is {b}")
+    sum=a+b
+    print(f"Sum is {sum}")
+
+# Call the function
+add()
+
+# While Loop in Python
+n=5
+while n>0:
+    print(n)
+    n-=1
+print("Thankyou for Visiting!")
---
+++ day_6_Project6.py
@@ -0,0 +1,6 @@
+# Project-6 = Escaping the Maze
+# In this Project we will use Reeborg's world website 
+import webbrowser
+url = "https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json"
+webbrowser.open(url)
+# Run This Program You will automatically visit the website
