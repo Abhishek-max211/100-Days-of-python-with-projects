3a3fb34c26fb6644b8c8ce2c723931caaae6d0b3
Abhishek Pundir <abhishek.pundir29nov@gmail.com>
2025-05-30 17:06:02 +0530

    Add files via upload

---
+++ data14.py
@@ -0,0 +1,50 @@
+data = [
+    {
+        'name':'Instagram',
+        'follower_count':346,
+        'description':'Social media platform',
+        'country':'United states'
+    },
+    {
+        'name':'Cristiano Ronaldo',
+        'follower_count':654,
+        'description':'Footballer',
+        'country':'Portugal'
+    },
+    {
+        'name':'Lionel Messi',
+        'follower_count':505,
+        'description':'Footballer',
+        'country':'Argentina'
+    },
+    {
+        'name':'MS Dhoni',
+        'follower_count':40,
+        'description':'Cricketer',
+        'country':'India'
+    },
+    {
+        'name':'Virat Kohli',
+        'follower_count':270,
+        'description':'Cricketer',
+        'country':'India'
+    },
+    {
+        'name':'Vin Diesel',
+        'follower_count':103,
+        'description':'American Actor',
+        'country':'Califormia'
+    },
+    {
+        'name':'Salman Khan',
+        'follower_count':70,
+        'description':'Indian Actor',
+        'country':'India'
+    },
+    {
+        'name':'Tom Cruise',
+        "follower_count":14,
+        'description':'American Actor',
+        'country':"America"
+    }
+]
